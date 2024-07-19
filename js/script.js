6dca400357c4a6f29ac693d2df375aec718bce92
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-07-19 21:49:50 +0200

    add code for changing rating bg color

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-let ratings = document.getElementsByClassName('ratings');
+let ul = document.querySelector('ul');
+let selectedLi;
 let submitBtn = document.getElementById('submitBtn');
 let ratingsValue;
 
 const submitRating = () => {
     // console.log(ratings)
+}
+
+ul.onclick =  function(e) {
+    if(e.target.tagName != 'LI') return;
+    selected(e.target);
+}
+
+const selected = (li) => {
+    if(selectedLi) {
+        selectedLi.classList.remove('rated');
+    }
+
+    selectedLi = li;
+    selectedLi.classList.add('rated');
 }
 
 submitBtn.addEventListener('click', submitRating);
