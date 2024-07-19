c6cd1a908c4b69a8d18865db0612c98933234264
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-07-19 21:16:08 +0200

    remove some js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 let ratings = document.getElementsByClassName('ratings');
 let submitBtn = document.getElementById('submitBtn');
+let ratingsValue;
 
 const submitRating = () => {
-    console.log(ratings)
+    // console.log(ratings)
 }
 
 submitBtn.addEventListener('click', submitRating);
